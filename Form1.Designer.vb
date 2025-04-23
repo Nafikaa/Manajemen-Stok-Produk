55ab6e539d9f10e1fb603bfaee8a094cbf059d82
Nafikaa <112855345+Nafikaa@users.noreply.github.com>
2025-04-24 03:09:18 +0700

    Add files via upload
    Desain mockup dan winform connect ke database sql.

---
+++ Form1.Designer.vb
@@ -0,0 +1,357 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txt_prono = New TextBox()
+        txt_Proname = New TextBox()
+        txt_price = New TextBox()
+        combo_proGroup = New ComboBox()
+        exp_datepicker = New DateTimePicker()
+        status_checkbox = New CheckBox()
+        btn_save = New Button()
+        btn_edit = New Button()
+        btn_delete = New Button()
+        btn_clear = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        DataGridView1 = New DataGridView()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Column4 = New DataGridViewTextBoxColumn()
+        Column5 = New DataGridViewTextBoxColumn()
+        Column6 = New DataGridViewCheckBoxColumn()
+        Label6 = New Label()
+        txt_search = New TextBox()
+        Label7 = New Label()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' txt_prono
+        ' 
+        txt_prono.Location = New Point(48, 131)
+        txt_prono.Name = "txt_prono"
+        txt_prono.Size = New Size(300, 31)
+        txt_prono.TabIndex = 0
+        ' 
+        ' txt_Proname
+        ' 
+        txt_Proname.Location = New Point(48, 193)
+        txt_Proname.Name = "txt_Proname"
+        txt_Proname.Size = New Size(300, 31)
+        txt_Proname.TabIndex = 1
+        ' 
+        ' txt_price
+        ' 
+        txt_price.Location = New Point(48, 255)
+        txt_price.Name = "txt_price"
+        txt_price.Size = New Size(300, 31)
+        txt_price.TabIndex = 2
+        ' 
+        ' combo_proGroup
+        ' 
+        combo_proGroup.FormattingEnabled = True
+        combo_proGroup.Items.AddRange(New Object() {"SWEETS", "COOL DRINKS", "FOODS"})
+        combo_proGroup.Location = New Point(48, 317)
+        combo_proGroup.Name = "combo_proGroup"
+        combo_proGroup.Size = New Size(300, 33)
+        combo_proGroup.TabIndex = 4
+        ' 
+        ' exp_datepicker
+        ' 
+        exp_datepicker.Location = New Point(48, 381)
+        exp_datepicker.Name = "exp_datepicker"
+        exp_datepicker.Size = New Size(300, 31)
+        exp_datepicker.TabIndex = 5
+        ' 
+        ' status_checkbox
+        ' 
+        status_checkbox.AutoSize = True
+        status_checkbox.Location = New Point(48, 418)
+        status_checkbox.Name = "status_checkbox"
+        status_checkbox.Size = New Size(109, 29)
+        status_checkbox.TabIndex = 6
+        status_checkbox.Text = "Available"
+        status_checkbox.UseVisualStyleBackColor = True
+        ' 
+        ' btn_save
+        ' 
+        btn_save.BackColor = Color.LimeGreen
+        btn_save.Cursor = Cursors.Hand
+        btn_save.FlatStyle = FlatStyle.Flat
+        btn_save.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_save.ForeColor = Color.White
+        btn_save.Location = New Point(48, 453)
+        btn_save.Name = "btn_save"
+        btn_save.Size = New Size(112, 34)
+        btn_save.TabIndex = 7
+        btn_save.Text = "Save"
+        btn_save.UseVisualStyleBackColor = False
+        ' 
+        ' btn_edit
+        ' 
+        btn_edit.BackColor = Color.DarkOrange
+        btn_edit.Cursor = Cursors.Hand
+        btn_edit.FlatStyle = FlatStyle.Flat
+        btn_edit.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_edit.ForeColor = Color.White
+        btn_edit.Location = New Point(236, 453)
+        btn_edit.Name = "btn_edit"
+        btn_edit.Size = New Size(112, 34)
+        btn_edit.TabIndex = 8
+        btn_edit.Text = "Update"
+        btn_edit.UseVisualStyleBackColor = False
+        ' 
+        ' btn_delete
+        ' 
+        btn_delete.BackColor = Color.Red
+        btn_delete.Cursor = Cursors.Hand
+        btn_delete.FlatStyle = FlatStyle.Flat
+        btn_delete.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_delete.ForeColor = Color.White
+        btn_delete.Location = New Point(48, 493)
+        btn_delete.Name = "btn_delete"
+        btn_delete.Size = New Size(112, 34)
+        btn_delete.TabIndex = 9
+        btn_delete.Text = "Delete"
+        btn_delete.UseVisualStyleBackColor = False
+        ' 
+        ' btn_clear
+        ' 
+        btn_clear.BackColor = Color.DarkSlateGray
+        btn_clear.Cursor = Cursors.Hand
+        btn_clear.FlatStyle = FlatStyle.Flat
+        btn_clear.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_clear.ForeColor = Color.White
+        btn_clear.Location = New Point(236, 493)
+        btn_clear.Name = "btn_clear"
+        btn_clear.Size = New Size(112, 34)
+        btn_clear.TabIndex = 10
+        btn_clear.Text = "Clear"
+        btn_clear.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(48, 103)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(97, 25)
+        Label1.TabIndex = 11
+        Label1.Text = "No.Produk"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(48, 165)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(121, 25)
+        Label2.TabIndex = 12
+        Label2.Text = "Nama Produk"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(48, 227)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(60, 25)
+        Label3.TabIndex = 13
+        Label3.Text = "Harga"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(48, 289)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(113, 25)
+        Label4.TabIndex = 14
+        Label4.Text = "Grup Produk"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(48, 353)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(112, 25)
+        Label5.TabIndex = 15
+        Label5.Text = "Expired Date"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.AllowUserToDeleteRows = False
+        DataGridView1.BackgroundColor = Color.White
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3, Column4, Column5, Column6})
+        DataGridView1.GridColor = SystemColors.ControlDark
+        DataGridView1.ImeMode = ImeMode.NoControl
+        DataGridView1.Location = New Point(382, 168)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.ReadOnly = True
+        DataGridView1.RowHeadersVisible = False
+        DataGridView1.RowHeadersWidth = 62
+        DataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        DataGridView1.Size = New Size(791, 359)
+        DataGridView1.TabIndex = 15
+        ' 
+        ' Column1
+        ' 
+        Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        Column1.HeaderText = "No. Produk"
+        Column1.MinimumWidth = 8
+        Column1.Name = "Column1"
+        Column1.ReadOnly = True
+        Column1.Width = 138
+        ' 
+        ' Column2
+        ' 
+        Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        Column2.HeaderText = "Nama Produk"
+        Column2.MinimumWidth = 8
+        Column2.Name = "Column2"
+        Column2.ReadOnly = True
+        Column2.Width = 157
+        ' 
+        ' Column3
+        ' 
+        Column3.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        Column3.HeaderText = "Harga"
+        Column3.MinimumWidth = 8
+        Column3.Name = "Column3"
+        Column3.ReadOnly = True
+        Column3.Width = 96
+        ' 
+        ' Column4
+        ' 
+        Column4.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        Column4.HeaderText = "Grup Produk"
+        Column4.MinimumWidth = 8
+        Column4.Name = "Column4"
+        Column4.ReadOnly = True
+        Column4.Width = 149
+        ' 
+        ' Column5
+        ' 
+        Column5.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        Column5.HeaderText = "Expired Date"
+        Column5.MinimumWidth = 8
+        Column5.Name = "Column5"
+        Column5.ReadOnly = True
+        Column5.Width = 148
+        ' 
+        ' Column6
+        ' 
+        Column6.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+        Column6.HeaderText = "Status"
+        Column6.MinimumWidth = 8
+        Column6.Name = "Column6"
+        Column6.ReadOnly = True
+        Column6.Width = 66
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(382, 103)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(64, 25)
+        Label6.TabIndex = 18
+        Label6.Text = "Search"
+        ' 
+        ' txt_search
+        ' 
+        txt_search.Location = New Point(382, 131)
+        txt_search.Name = "txt_search"
+        txt_search.Size = New Size(408, 31)
+        txt_search.TabIndex = 16
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 15F, FontStyle.Bold)
+        Label7.Location = New Point(363, 29)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(485, 41)
+        Label7.TabIndex = 19
+        Label7.Text = "Aplikasi Manajemen Stok Produk"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1214, 575)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(txt_search)
+        Controls.Add(DataGridView1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btn_clear)
+        Controls.Add(btn_delete)
+        Controls.Add(btn_edit)
+        Controls.Add(btn_save)
+        Controls.Add(status_checkbox)
+        Controls.Add(exp_datepicker)
+        Controls.Add(combo_proGroup)
+        Controls.Add(txt_price)
+        Controls.Add(txt_Proname)
+        Controls.Add(txt_prono)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txt_prono As TextBox
+    Friend WithEvents txt_Proname As TextBox
+    Friend WithEvents txt_price As TextBox
+    Friend WithEvents combo_proGroup As ComboBox
+    Friend WithEvents exp_datepicker As DateTimePicker
+    Friend WithEvents status_checkbox As CheckBox
+    Friend WithEvents btn_save As Button
+    Friend WithEvents btn_edit As Button
+    Friend WithEvents btn_delete As Button
+    Friend WithEvents btn_clear As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txt_search As TextBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewCheckBoxColumn
+    Friend WithEvents Label7 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,169 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    ' Inisialisasi koneksi ke database MySQL
+    Dim conn As New MySqlConnection("server=localhost;port=3306;username=root;password=;database=curd")
+    Dim dr As MySqlDataReader
+
+    ' Saat form dimuat, data langsung ditampilkan di DataGridView
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DGV_load()
+    End Sub
+
+    ' Tombol Simpan untuk menyimpan data ke tabel tbl_curd
+    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        Dim cmd As New MySqlCommand("INSERT INTO `tbl_curd`(`NOPRODUK`,`NAMAPRODUK`, `HARGA`, `GRUP`, `EXPDATE`, `STATUS`) VALUES (@NOPRODUK,@NAMAPRODUK,@HARGA,@GRUP,@EXPDATE,@STATUS)", conn)
+        ' Mengisi parameter dengan data dari form
+        cmd.Parameters.AddWithValue("@NOPRODUK", txt_prono.Text)
+        cmd.Parameters.AddWithValue("@NAMAPRODUK", txt_Proname.Text)
+        cmd.Parameters.AddWithValue("@HARGA", CDec(txt_price.Text))
+        cmd.Parameters.AddWithValue("@GRUP", combo_proGroup.Text)
+        cmd.Parameters.AddWithValue("@EXPDATE", CDate(exp_datepicker.Value))
+        cmd.Parameters.AddWithValue("@STATUS", status_checkbox.Checked)
+
+        Try
+            ' Menjalankan perintah INSERT
+            conn.Open()
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Record Save Success", "CURD", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            ' Menangani error saat menyimpan
+            MessageBox.Show("Record Save Failed: " & ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+        Finally
+            conn.Close()
+        End Try
+
+        ' Setelah simpan, bersihkan form dan refresh data
+        clear()
+        DGV_load()
+    End Sub
+
+    ' Prosedur untuk memuat data dari database ke DataGridView
+    Private Sub DGV_load()
+        DataGridView1.Rows.Clear()
+        Try
+            conn.Open()
+            Dim cmd As New MySqlCommand("SELECT * FROM `tbl_curd`", conn)
+            dr = cmd.ExecuteReader
+            While dr.Read
+                ' Tambahkan setiap baris dari hasil query ke DataGridView
+                DataGridView1.Rows.Add(dr.Item("NOPRODUK"), dr.Item("NAMAPRODUK"), dr.Item("HARGA"), dr.Item("GRUP"), dr.Item("EXPDATE"), Format(CBool(dr.Item("STATUS"))))
+            End While
+            dr.Close()
+        Catch ex As Exception
+            MsgBox("Error loading data: " & ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    ' Tombol untuk menghapus isian form
+    Private Sub btn_clear_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
+        clear()
+    End Sub
+
+    Private Sub clear()
+        txt_prono.Clear()
+        txt_Proname.Clear()
+        txt_price.Clear()
+        combo_proGroup.Text = ""
+        exp_datepicker.Value = DateTime.Now
+        status_checkbox.CheckState = CheckState.Unchecked
+
+        ' Aktifkan kembali textbox dan tombol simpan
+        txt_prono.ReadOnly = False
+        btn_save.Enabled = True
+    End Sub
+
+    ' Saat pengguna mengklik data di DataGridView, tampilkan di input form
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        txt_prono.Text = DataGridView1.CurrentRow.Cells(0).Value
+        txt_Proname.Text = DataGridView1.CurrentRow.Cells(1).Value
+        txt_price.Text = DataGridView1.CurrentRow.Cells(2).Value
+        combo_proGroup.Text = DataGridView1.CurrentRow.Cells(3).Value
+        exp_datepicker.Text = DataGridView1.CurrentRow.Cells(4).Value
+        status_checkbox.Checked = DataGridView1.CurrentRow.Cells(5).Value
+
+        ' Buat NOPRODUK tidak bisa diubah, dan matikan tombol simpan
+        txt_prono.ReadOnly = True
+        btn_save.Enabled = False
+    End Sub
+
+    ' Prosedur untuk mengedit data yang dipilih
+    Sub Edit()
+        Dim cmd As New MySqlCommand("UPDATE `tbl_curd` SET `NAMAPRODUK` = @NAMAPRODUK, `HARGA` = @HARGA, `GRUP` = @GRUP, `EXPDATE` = @EXPDATE, `STATUS` = @STATUS WHERE `NOPRODUK` = @NOPRODUK", conn)
+        cmd.Parameters.Clear()
+        cmd.Parameters.AddWithValue("@NOPRODUK", txt_prono.Text)
+        cmd.Parameters.AddWithValue("@NAMAPRODUK", txt_Proname.Text)
+        cmd.Parameters.AddWithValue("@HARGA", CDec(txt_price.Text))
+        cmd.Parameters.AddWithValue("@GRUP", combo_proGroup.Text)
+        cmd.Parameters.AddWithValue("@EXPDATE", CDate(exp_datepicker.Value))
+        cmd.Parameters.AddWithValue("@STATUS", status_checkbox.Checked)
+
+        Try
+            conn.Open()
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Record Update Success", "CURD", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("Record Update Failed: " & ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+        Finally
+            conn.Close()
+        End Try
+
+        clear()
+        DGV_load()
+    End Sub
+
+    ' Tombol untuk memanggil prosedur Edit
+    Private Sub btn_edit_Click(sender As Object, e As EventArgs) Handles btn_edit.Click
+        Edit()
+    End Sub
+
+    ' Prosedur untuk menghapus data
+    Private Sub Delete()
+        If MsgBox("Are you sure you want to delete this record?", MsgBoxStyle.YesNo, "Delete") = MsgBoxResult.Yes Then
+            Dim cmd As New MySqlCommand("DELETE FROM `tbl_curd` WHERE `NOPRODUK` = @NOPRODUK", conn)
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@NOPRODUK", txt_prono.Text)
+            Try
+                conn.Open()
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Record Delete Success", "CURD", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Record Delete Failed: " & ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            Finally
+                conn.Close()
+            End Try
+            clear()
+            DGV_load()
+        End If
+    End Sub
+
+    ' Tombol hapus memanggil prosedur Delete
+    Private Sub btn_delete_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
+        Delete()
+    End Sub
+
+    ' Prosedur pencarian data berdasarkan NOPRODUK atau NAMAPRODUK
+    Private Sub txt_search_TextChanged(sender As Object, e As EventArgs) Handles txt_search.TextChanged
+        DataGridView1.Rows.Clear()
+        Try
+            conn.Open()
+            Dim cmd As New MySqlCommand("SELECT * FROM `tbl_curd` WHERE `NOPRODUK` LIKE @search OR `NAMAPRODUK` LIKE @search", conn)
+            cmd.Parameters.AddWithValue("@search", "%" & txt_search.Text & "%")
+            dr = cmd.ExecuteReader
+            While dr.Read
+                DataGridView1.Rows.Add(dr.Item("NOPRODUK"), dr.Item("NAMAPRODUK"), dr.Item("HARGA"), dr.Item("GRUP"), dr.Item("EXPDATE"), Format(CBool(dr.Item("STATUS"))))
+            End While
+            dr.Close()
+        Catch ex As Exception
+            MsgBox("Error loading data: " & ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+End Class
+
+
